3d8304b812bce04ef23478506a720faf4376f142
Adphi <philippe.adrien.nousse@gmail.com>
2021-12-05 15:51:19 +0100

    defaults to build without ceph

--- nbd/rbd.go
+++ nbd/rbd.go
@@ -1,10 +1,7 @@
-// +build linux,!noceph
+// +build ceph
 
-// The above build tag specifies this file is only to be built on linux (because
-// building librados is difficult elsewhere). If you want it to build elsewhere,
-// add your OS as appropriately. If you are having difficulty building on linux
-// or want to build without librados present, then use
-//    go build -tags 'noceph'
+// If you want to build with librados present, then use
+//    go build -tags 'ceph'
 
 package nbd
 
